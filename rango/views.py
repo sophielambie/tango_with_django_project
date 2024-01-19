4de837d96749de0b3fba468b92ceef4637a5eaa0
Sophie Lambie <2764964l@student.gla.ac.uk>
2024-01-19 17:01:07 +0000

    until end of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Link to about page: <a href='/rango/about'> About</a>")
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. Link back to home: <a href='/rango/'>Index</a>")
+    context_dict = {"boldmessage": "This tutuorial has been put together by Sophie"}
+    return render(request, 'rango/about.html', context=context_dict)
+
+#Rango says here is the about page. Link back to home: <a href='/rango/'>Index</a>"
