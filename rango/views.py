877ba7d75788dd42a64bcc5f49455e7103051236
Sophie Lambie <2764964l@student.gla.ac.uk>
2024-02-07 22:21:58 +0000

    chapter 10 ex done and tests passed but im not convinced it works

--- rango/views.py
+++ rango/views.py
@@ -17,7 +17,7 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
-    context_dict['boldmessage'] = "Crunchy, creamy, cookie, candy, cupcake!"
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
@@ -29,13 +29,13 @@ def index(request):
 
 
 def about(request):
-    context_dict = {"boldmessage": "This tutuorial has been put together by Sophie"}
+    context_dict = {"boldmessage": "This tutorial has been put together by Sophie"}
 
-    if request.session.test_cookie_worked(): 
-        print("TEST COOKIE WORKED!") 
-        request.session.delete_test_cookie()
+    #visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
-    return render(request, 'rango/about.html', context=context_dict)
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -172,9 +172,11 @@ def visitor_cookie_handler(request):
     
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
 
     else:
-
-        request.session['visits'] = visits
+        request.session['last_visit'] = last_visit_cookie
         
+    request.session['visits'] = visits
+
 
